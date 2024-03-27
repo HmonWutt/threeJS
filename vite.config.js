1c5419efe6ac257ce2810e7d56129b08226f9c14
hmon <wu2504hm-s@student.lu.se>
2024-03-27 14:06:58 +0100

    change base

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/19-scroll-based-animation/",
+  base: "/threeJS/",
 });
